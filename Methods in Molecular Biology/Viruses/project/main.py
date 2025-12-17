2ef9387b0c0817c6ea6a6d4ec934106ef018de7f
Asif Ali <maa32@illinois.edu>
2025-12-16 19:01:35 -0600

    Update main.py

--- Methods in Molecular Biology/Viruses/project/main.py
+++ Methods in Molecular Biology/Viruses/project/main.py
@@ -1,12 +1,12 @@
 #This file will serve as the base for the project calling important functions from other scripts
-from functions import get_rmsd_hdr, get_iupred
+from functions import get_rmsd_hdr, get_iupred, get_regions, get_slices
 
 def main():
     labels, rmsds = get_rmsd_hdr('data/rmsd_hdr/')
     labels, disorder, binding = get_iupred('data/iupred/')
-    print(rmsds)
-    print(disorder)
-    print(binding)
+    regions = get_regions(rmsds,3.0)
+    slices = get_slices(regions,'data/pdbs/', 'A', 'data/sliced_pdbs/')
+    
 
 if __name__=="__main__":
     main()
