ca66a9ef4d17cf981f5717bbf94ebab066461100
Asif Ali <maa32@illinois.edu>
2025-12-20 14:12:51 -0600

    Update main.py

--- Methods in Molecular Biology/Viruses/project/main.py
+++ Methods in Molecular Biology/Viruses/project/main.py
@@ -1,21 +1,14 @@
 #This file will serve as the base for the project calling important functions from other scripts
 from functions import get_rmsd_hdr, get_iupred, get_regions, get_slices, get_mut_fasta, get_tm_scores
+from visualize import get_tm_fig, get_rmsdhdr_fig, get_iupred_anchor_fig
+import pandas as pd
 
 def main():
-    '''
-    get_mut_fasta('data/fasta/Spike_Wuhan.fasta','data/mutations/','data/fasta/')
-    labels, rmsds = get_rmsd_hdr('data/rmsd_hdr/')
-    labels, disorder, binding = get_iupred('data/iupred/')
-    regions = get_regions(rmsds,3.0)
-    slices = get_slices(regions,'data/pdbs/', 'A', 'data/sliced_pdbs/')
-    get_tm_scores('data/us_align/test/')
-    labels, rmsds = get_rmsd_hdr('data/rmsd_hdr/','data/processed_scores/')
-    regions = get_regions(rmsds,3.0,'data/processed_scores/')
-    labels, disorder, binding = get_iupred('data/iupred/', 'data/processed_scores/')
-    '''
-    grps = get_tm_scores('data/us_align/test/', 'data/processed_scores/')
-    
+
+    get_iupred_anchor_fig('data/processed_scores/anchor2_scores.csv', 'data/figs/')
+    get_iupred_anchor_fig('data/processed_scores/iupred3_scores.csv', 'data/figs/')
+    get_rmsdhdr_fig('data/processed_scores/rmsd_headers.csv', 'data/figs/')
+    get_tm_fig('data/processed_scores/tm_scores.csv', 'data/figs/')
     
 if __name__=="__main__":
     main()
-
